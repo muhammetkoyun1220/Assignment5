0cd834a3072fd9d8586b770fc8bcbdebe268de8d
muhammetkoyun1220 <126719242+muhammetkoyun1220@users.noreply.github.com>
2023-04-09 17:46:04 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Activity5-CurrencyCalculator</title>
+</head>
+<body>
+
+  <?php
+      
+      $currencyLists=[
+        ['name'=>'USD','value'=>2.7],
+        ['name'=>'CAD','value'=>2.0],
+        ['name'=>'EUR','value'=>3.0]
+      ];
+
+
+      $from=0;
+      $curency1 = 0;
+      $curency2 = 0;
+    if (isset($_POST['Convert'])) {
+      $from =$_POST['From'];
+      $curency1 = $_POST['Currency1'];
+      $curency2 = $_POST['Currency2'];
+      $result=$from*$curency1/$curency2;
+    }
+
+   ?>
+
+
+<form method="post">
+
+ <label for="From">From:</label>
+  <input type="text" id="From" name="From" value="<?php echo $from??0 ?>">
+
+
+  <label for="Currency">Currency:</label>
+  <select id="Currency1" name="Currency1">
+    <?php foreach($currencyLists as $item) { ?>
+    <option <?php echo $item['value']==$curency1?'selected':'' ?>  value="<?php echo $item['value'] ?>"><?php echo $item['name']?></option>
+  <?php } ?>
+  </select>
+
+  <br><br>
+
+  <label for="To">To:</label>
+  <input type="text" id="To" name="To" value="<?php echo $result??0 ?>">
+
+
+  <label for="Currency">Currency:</label>
+  <select id="Currency2" name="Currency2">
+    <?php foreach($currencyLists as $item) { ?>
+    <option <?php echo $item['value']==$curency2?'selected':'' ?> value="<?php echo $item['value'] ?>"><?php echo $item['name'] ?></option>
+  <?php } ?>
+  </select>
+
+
+  <br><br>
+
+
+  <input type="submit" name="Convert" value="Convert">
+  
+  </form>
+
+</body>
+</html>
